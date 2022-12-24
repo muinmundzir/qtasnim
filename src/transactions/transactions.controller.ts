37bf7f778888151de3ccb5b55fff4eeba517f93a
Cloud <muin@docheck.id>
2022-12-24 18:35:38 +0700

    feat: add filter query

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
@@ -30,10 +31,23 @@ export class TransactionsController {
   }
 
   @Get()
-  async findAll() {
-    const transactionsData = await this.transactionsService.findAll();
+  async findAll(
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+    @Query('itemName') itemName: string,
+  ) {
+    let query = await this.transactionsService.findAll();
+    if (startDate && endDate) {
+      query = await this.transactionsService.findBetweenDate(
+        startDate,
+        endDate,
+      );
+    }
 
-    const transactions: TransactionInterface[] = transactionsData.map(
+    if (itemName) query = await this.transactionsService.findByName(itemName);
+
+    const transactionsData = query;
+    const transactions: TransactionInterface[] = transactionsData?.map(
       (transactionData) => {
         return {
           id: transactionData.id,
@@ -52,6 +66,30 @@ export class TransactionsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.transactionsService.findOne(+id);
+  }
+
+  @Get('filterDate')
+  async findBetween(@Query('startDate') startDate: string) {
+    console.log(startDate, 'query');
+    return startDate;
+    // const transactionsData = await this.transactionsService.findBetweenDate(
+    //   query,
+    // );
+
+    // const transactions: TransactionInterface[] = transactionsData.map(
+    //   (transactionData) => {
+    //     return {
+    //       id: transactionData.id,
+    //       itemName: transactionData.item.name,
+    //       stock: transactionData.item.stock,
+    //       soldAmount: transactionData.amount,
+    //       transactionDate: transactionData.transactionDate,
+    //       itemType: transactionData.item.type,
+    //     };
+    //   },
+    // );
+    //
+    // return transactions;
   }
 
   @Patch(':id')
