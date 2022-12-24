425d77614cef19180c1f653fcefccabdbbff6160
Cloud <muin@docheck.id>
2022-12-24 10:51:20 +0700

    feat: add transactions resource

---
+++ src/transactions/transactions.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { Transaction } from './entities/transaction.entity';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    @InjectRepository(Transaction)
+    private transactionRepository: Repository<Transaction>,
+  ) {}
+  create(createTransactionDto: CreateTransactionDto) {
+    const newTransaction =
+      this.transactionRepository.create(createTransactionDto);
+
+    return this.transactionRepository.save(newTransaction);
+  }
+
+  findAll() {
+    return `This action returns all transactions`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} transaction`;
+  }
+
+  update(id: number, updateTransactionDto: UpdateTransactionDto) {
+    return `This action updates a #${id} transaction`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} transaction`;
+  }
+}
