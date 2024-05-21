ef28488c910a6dd654d7aa8966a94ec0d4fab005
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-05-21 19:08:42 +0700

    feat: implement validation input

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateItemDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   stock: number;
+
+  @IsNotEmpty()
   type: string;
 }
