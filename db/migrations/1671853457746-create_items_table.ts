58dad82558d9927a11e7746e068f26a03a354f41
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-05-21 19:07:45 +0700

    chore: update migration file

--- db/migrations/1671853457746-create_items_table.ts
+++ db/migrations/1671853457746-create_items_table.ts
@@ -5,13 +5,13 @@ export class createItemsTable1671853457746 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "transaction" ("id" SERIAL NOT NULL, "amount" integer NOT NULL, "deleted_at" TIMESTAMP, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "item" integer, CONSTRAINT "PK_89eadb93a89810556e1cbcd6ab9" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "transaction" ("id" SERIAL NOT NULL, "stock_history" integer NOT NULL, "amount" integer NOT NULL, "deleted_at" TIMESTAMP, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "item" integer, CONSTRAINT "PK_89eadb93a89810556e1cbcd6ab9" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "item" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "stock" integer NOT NULL, "type" character varying NOT NULL, "deleted_at" TIMESTAMP, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `ALTER TABLE "transaction" ADD CONSTRAINT "FK_a30b66488fbc39ca1b5ac71e2a6" FOREIGN KEY ("item") REFERENCES "item"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+      `ALTER TABLE "transaction" ADD CONSTRAINT "FK_a30b66488fbc39ca1b5ac71e2a6" FOREIGN KEY ("item") REFERENCES "item"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
   }
 
