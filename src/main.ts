8e1291227c60720689403ce91da07f61c50ab233
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-05-21 19:08:24 +0700

    feat: setup validation setting

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
